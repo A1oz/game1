122d2d9bca6fc0cea45daa4e9b12b800c1812fc8
A1oz <107599705+A1oz@users.noreply.github.com>
2022-06-16 11:00:33 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1655351932,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/frame-sheet0.png",
+		"images/car-sheet0.png",
+		"images/tree-sheet0.png",
+		"images/star-sheet0.png",
+		"images/boomb-sheet0.png",
+		"images/gameover-sheet0.png",
+		"images/play-sheet0.png",
+		"images/background-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
